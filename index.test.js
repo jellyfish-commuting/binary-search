24e1f15483e2a418ab9fc5e39cef4ac4cae05d70
Mohamed B <mohamed@jellyfish-commuting.com>
2020-03-26 11:31:40 +0100

    Add test when range error - Remove unnecessary 'async'

--- index.test.js
+++ index.test.js
@@ -6,17 +6,19 @@ const letters = new Array(26).fill().map((nop, i) => String.fromCharCode(97 + i)
 
 // Start tests ...
 describe('Equal search', () => {
-  it('expect -1 when searching 150 in [0...99]', async () => expect(equalSearch(numbers, 150)).toBe(-1));
-  it('expect 7 when searching 7 in [0...99]', async () => expect(equalSearch(numbers, 7)).toBe(7));
-  it('expect -1 when searching \'thejellyfish\' in [\'a\'...\'z\']', async () => expect(equalSearch(letters, 'thejellyfish')).toBe(-1));
-  it('expect 7 when searching \'h\' in [\'a\'...\'z\']', async () => expect(equalSearch(letters, 'h')).toBe(7));
+  it('expect -1 when searching 150 in [0...99]', () => expect(equalSearch(numbers, 150)).toBe(-1));
+  it('expect 7 when searching 7 in [0...99]', () => expect(equalSearch(numbers, 7)).toBe(7));
+  it('expect -1 when searching \'thejellyfish\' in [\'a\'...\'z\']', () => expect(equalSearch(letters, 'thejellyfish')).toBe(-1));
+  it('expect 7 when searching \'h\' in [\'a\'...\'z\']', () => expect(equalSearch(letters, 'h')).toBe(7));
+  it('expect error when outbounds range', () => expect(() => equalSearch(numbers, 7, { to: 101 })).toThrow(RangeError));
 });
 
 describe('Closest search', () => {
-  it('expect 0 when searching -1 in [0...99]', async () => expect(closestSearch(numbers, -1)).toBe(0));
-  it('expect 99 when searching 101 in [0...99]', async () => expect(closestSearch(numbers, 100)).toBe(99));
-  it('expect 7 when searching 7 in [0...99]', async () => expect(closestSearch(numbers, 7)).toBe(7));
-  it('expect 7 when searching 7.4 in [0...99]', async () => expect(closestSearch(numbers, 7.4)).toBe(7));
-  it('expect 77 when searching 77 in [0...99]', async () => expect(closestSearch(numbers, 77)).toBe(77));
-  it('expect 77 when searching 77.4 in [0...99]', async () => expect(closestSearch(numbers, 77.4)).toBe(77));
+  it('expect 0 when searching -1 in [0...99]', () => expect(closestSearch(numbers, -1)).toBe(0));
+  it('expect 99 when searching 101 in [0...99]', () => expect(closestSearch(numbers, 100)).toBe(99));
+  it('expect 7 when searching 7 in [0...99]', () => expect(closestSearch(numbers, 7)).toBe(7));
+  it('expect 7 when searching 7.4 in [0...99]', () => expect(closestSearch(numbers, 7.4)).toBe(7));
+  it('expect 77 when searching 77 in [0...99]', () => expect(closestSearch(numbers, 77)).toBe(77));
+  it('expect 77 when searching 77.4 in [0...99]', () => expect(closestSearch(numbers, 77.4)).toBe(77));
+  it('expect error when outbounds range', () => expect(() => closestSearch(numbers, 7, { to: 101 })).toThrow(RangeError));
 });
